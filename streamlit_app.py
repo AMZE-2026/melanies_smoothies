987c87ddfb9386c57a543e07741f97677a1ebe90
AMZE-2026 <amina.zerkdi@axys-consultants.fr>
2026-07-09 11:30:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,4 @@ if ingredients_list : # this means if the list is not empty
 # new section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)    
+st.text(smoothiefroot_response.json())    
