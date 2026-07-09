05205c72c25fb70b821d82ef63596f87a9117f43
AMZE-2026 <amina.zerkdi@axys-consultants.fr>
2026-07-09 11:32:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,6 @@ if ingredients_list : # this means if the list is not empty
 # new section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())    
+#st.text(smoothiefroot_response.json())  
+#putting the data into a dataframe
+sf_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
